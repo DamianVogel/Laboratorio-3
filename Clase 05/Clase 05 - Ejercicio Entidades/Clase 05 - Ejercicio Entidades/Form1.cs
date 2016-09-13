44c5f21392856da27b262062e81f788f2cd02b6a
DamianVogel <rodolfovogel@gmail.com>
2016-09-13 20:23:30 -0300

    Clase 05 - Ejercicio a medio hacer.

---
+++ Clase 05/Clase 05 - Ejercicio Entidades/Clase 05 - Ejercicio Entidades/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidad;
+
+namespace Clase_05___Ejercicio_Entidades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Class1 numero1 = new Class1(1, "juan", 10);
+            Class1 numero2 = new Class1(2, "pepe", 20);
+            Class1 numero3 = new Class1(3, "pelota", 30);
+            Class1 numero4 = new Class1(4, "julian", 40);
+            Class1 numero5 = new Class1(5, "beto", 50);
+            Class1 numero6 = new Class1(6, "carlos", 60);
+            Class1 numero7 = new Class1(7, "enrique", 70);
+            Class1 numero8 = new Class1(8, "caribe", 80);
+            Class1 numero9 = new Class1(9, "xuabro", 90);
+            Class1 numero10 = new Class1(10, "Chamho", 100);
+
+            Class1[] lista = new Class1[10];
+            lista.SetValue(numero1, 0);
+            lista.SetValue(numero2, 1);
+            lista.SetValue(numero3, 2);
+            lista.SetValue(numero4, 3);
+            lista.SetValue(numero5, 4);
+            lista.SetValue(numero6, 5);
+            lista.SetValue(numero7, 6);
+            lista.SetValue(numero8, 7);
+            lista.SetValue(numero9, 8);
+            lista.SetValue(numero10, 9);       
+      
+            Array.Sort(lista,Entidad.Class1.CompararPrecio);
+
+            this.listBox1.Items.AddRange(lista);
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La informacion es: " + this.listBox1.Items[0].ToString());
+        }
+
+       
+        
+      
+        
+            
+
+    
+    }
+}
---
+++ Clase 05/Clase 05 - Ejercicio Entidades/Entidad/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Class1
+    {
+        private int _cantidad;
+        private string _marca;
+        private int _precio;
+
+        public Class1(int cantidad, string marca, int precio)
+        {
+            this._cantidad = cantidad;
+            this._marca = marca;
+            this._precio = precio;
+        }
+
+        public int Cantidad { get { return this._cantidad; } set {this._cantidad=value;} }
+        public string Marca { get { return this._marca; } set { this._marca = value; } }
+        public int Precio { get { return this._precio; } set {this._precio=value;} }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("La cantidad es:");
+            sb.AppendLine(this._cantidad.ToString());
+            sb.Append("La marca es:");
+            sb.AppendLine(this._marca);
+            sb.Append("El precio es:");
+            sb.AppendLine(this._precio.ToString());
+
+            return sb.ToString();
+
+
+        }
+
+        public static int CompararPrecio(Class1 a, Class1 b)
+        {
+            int c = 2;
+
+            if (a._precio > b._precio)
+            {
+                c = 1;
+            }
+
+            if (a._precio < b._precio)
+            {
+                c = -1;
+            }
+
+            if (a._precio == b._precio)
+            {
+                c = 0;
+            }
+
+
+
+            return c;
+        }
+
+        
+
+
+
+    
+    }
+}
