517657ec8ab6f6ae2ac3f2b0cc93bfc6c691b42a
DamianVogel <rodolfovogel@gmail.com>
2016-09-14 19:00:12 -0300

    Probar metodo de ordenamiento de string

--- Ejercicios - Solucion/Aplicacion.03 - Clase01/Form1.cs
+++ Ejercicios - Solucion/Aplicacion.03 - Clase01/Form1.cs
@@ -29,10 +29,43 @@ namespace Aplicacion._03
                 cadenaRev = String.Concat(cadenaRev, cadena.Substring(((cadena.Length-1)-i),1));
             }
 
-            MessageBox.Show(cadenaRev);
-           
+            this.txtInvertida.Text = cadenaRev;
+
+            
+
+            
+
+
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.txtInvertida.ReadOnly = true;
+            this.txtAlfabetico.ReadOnly = true;
+        
+        }
+
+        public static string OrdenarCadena (string a)
+        {
+                  
+            for (int i = 0; i < a.Length-1; i++)
+            {
+                for (int j = i+1; j < a.Length; j++)
+                {
+                    if (char.Parse(a.Substring(i, 1)) > char.Parse(a.Substring(j, 1)))
+                    {
+                            string c = a.Substring(i, 1);
+                            a.Insert(i, a.Substring(j, 1).ToString());
+                            a.Insert(j,c);
+                    }
+
+                    
+                              
+                }           
+            }
+            //PROBAR ESTE METODO!
+            return a;     
+        }
        
     }
 }
