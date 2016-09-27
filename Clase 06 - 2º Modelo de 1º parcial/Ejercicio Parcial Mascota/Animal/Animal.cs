6d6f3654b9c928f493c6ecbfc7de1f933c16681b
DamianVogel <rodolfovogel@gmail.com>
2016-09-27 15:28:54 -0300

    Arranque con el segundo modelo de parcial

---
+++ Clase 06 - 2º Modelo de 1º parcial/Ejercicio Parcial Mascota/Animal/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascota
+{
+    public class Animal
+    {
+        protected int _edad;
+
+        public Animal(int edad)
+        {
+            this._edad = edad;
+        }
+
+        public int Edad { get{return this._edad;} }
+
+        public override string ToString()
+        {
+            return this._edad.ToString();
+        }
+    }
+}
---
+++ Clase 06 - 2º Modelo de 1º parcial/Ejercicio Parcial Mascota/Animal/Mascota.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Mascota
+{
+    class Mascota:Animal
+    {
+        protected string _nombre;
+        protected eTipoDeMascota _tipoDeMascota;
+
+
+        public Mascota(string nombre, eTipoDeMascota tipo, int edad)
+            : base(edad)
+        {
+            this._nombre = nombre;
+            this._tipoDeMascota = tipo;
+            
+        }
+
+        public string Nombre { get {return this._nombre; }  }
+        public eTipoDeMascota TipoDeMascota { get { return this._tipoDeMascota; } }
+
+
+        public int OrdenarPorEdad(Mascota mascotaUno, Mascota mascotaDos)
+        {
+            if (mascotaUno._edad > mascotaDos._edad)
+            {
+                return 1;
+            }
+
+            if (mascotaUno._edad < mascotaDos._edad)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        public int OrdenarPorNombre(Mascota mascotaUno, Mascota mascotaDos)
+        {
+
+            return String.Compare(mascotaUno._nombre, mascotaDos._nombre);
+            
+        }
+
+
+        public int OrdenarPorTipo(Mascota mascotaUno, Mascota mascotaDos)
+        {
+
+            return String.Compare(mascotaUno._tipoDeMascota.ToString(), mascotaDos._tipoDeMascota.ToString());
+            
+            
+        }
+
+        public override string ToString()
+        {
+            return this._nombre + " " + this._edad.ToString() + " " + this._tipoDeMascota.ToString();
+        }
+
+    
+    
+    }
+}
