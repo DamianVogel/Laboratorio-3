ef683cfee60fc0212bf916d81b80e46c0a4d5dea
DamianVogel <rodolfovogel@gmail.com>
2016-10-05 18:27:49 -0300

    Probando controles!

---
+++ PruebaControles/PruebaControles/PruebaTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaControles
+{
+    public class PruebaTextBox : System.Windows.Forms.TextBox
+    {
+        public PruebaTextBox()
+        {
+            this.BackColor = System.Drawing.Color.CadetBlue;
+
+
+        }
+
+        public PruebaTextBox(System.Drawing.Color color)
+        {
+            this.BackColor = color;
+        }
+    
+    }
+}
---
+++ PruebaControles/PruebaForm/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace PruebaForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pruebaTextBox1 = new PruebaControles.PruebaTextBox();
+            this.SuspendLayout();
+            // 
+            // pruebaTextBox1
+            // 
+            this.pruebaTextBox1.BackColor = System.Drawing.Color.CadetBlue;
+            this.pruebaTextBox1.Location = new System.Drawing.Point(99, 83);
+            this.pruebaTextBox1.Name = "pruebaTextBox1";
+            this.pruebaTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.pruebaTextBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pruebaTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PruebaControles.PruebaTextBox pruebaTextBox1;
+    }
+}
+
